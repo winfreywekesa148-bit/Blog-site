230183e42998c713ffe13d41aff2c2cd2ed7c6fb
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-05-05 11:26:59 +0300

    Add image URL and initial blog post
    Added an image URL and a blog post about pizza.

--- Blog.js
+++ Blog.js
@@ -1,5 +1,13 @@
 const blog = {
     name: "Restraurant Review",
     description: "A blog about restaurant reviews and food experiences."
+    image: "https://www.pinterest.com/pin/38351034331772561/",
+    posts: [
+        {
+            title: "The Best Pizza in Town",
+            content: "I recently visited Joe's Pizza and it was amazing! The crust was crispy and the toppings were fresh. Highly recommend!",
+        }
+    ]
+};
     
 };
